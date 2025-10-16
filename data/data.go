cf5eb671c6649d3220c715ceedf1097ec3375ee7
berylatieno <berylatieno30@gmail.com>
2025-10-16 08:04:04 +0300

    fetch cat facts

--- data/data.go
+++ data/data.go
@@ -1,10 +1,51 @@
 package data
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 type APIResponse struct {
 	Fact   string `json:"fact"`
 	Length int    `json:"length"`
 }
 
-func FetchCatFacts() {
+func FetchCatFacts() string {
+	url := "https://catfact.ninja/fact"
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("error fetching cat fact:", err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Print("Error: unexpected status code: ", response.StatusCode)
+		os.Exit(1)
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("error reading response body: ", err)
+		return ""
+	}
+
+	var catFactResponse APIResponse
+
+	err = json.Unmarshal(body, &catFactResponse)
+
+	if err != nil {
+		fmt.Println("error parsing JSON: ", err)
+		return ""
+	}
+
+	return catFactResponse.Fact
 
 }
